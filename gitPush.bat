86422b8676f5b0c65f1527d979a2ddd229ac6f38
Rafael <raafaellacerdaa@gmail.com>
2024-07-16 01:31:26 -0300

    Atualizado

--- gitPush.bat
+++ gitPush.bat
@@ -1,7 +1,12 @@
 echo || "Iniciando"
 
 git init
-git add .
+git add assets
+git add gitPush.bat
+git add main.py
+git add README.MD
+git add req.py
+git add requirements.txt
 git status
 git commit -m "Atualizado"
 git branch -M main
